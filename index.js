4ee79ca9ebfc80c354b01e4a6106363a71ba6371
Davidoffili <palagashvilid@gmail.com>
2020-06-28 18:26:02 -0700

    adapted index.js from api-server, serves the same purpose here

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 'use strict';
+// NOTE: this file is used to start our app.
+
+// require dotenv and the mongoose library.
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+// reading PORT from our .env file
+const PORT = process.env.PORT || 3001;
+
+// require lib/server.js
+const server = require('./lib/server.js');
+
+// connect mongo database, open a default mongoose connection
+mongoose.connect(process.env.MONGODB_ATLAS_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Prior to calling start() on our imported server module, it connects to Mongo, via mongoose.
+
+const dataBase = mongoose.connection;
+
+dataBase.on('open', () => {
+  console.log('Now connected to Mongo Database');
+});
+
+// call the .start() method from the server with the PORT set in your environment
+server.start(PORT);
